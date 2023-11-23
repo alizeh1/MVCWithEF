be82f3435a80dcacfe872803701b7599b2f3966e
AlishaMaljapte <alishamaljapte@stspl.com>
2023-11-23 11:32:36 +0530

    My first commit

---
+++ MVCCrudWithEF/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using MVCCrudWithEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrudWithEF.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        private readonly EmployeeEntities _dbContext = new EmployeeEntities();
+        
+        public ActionResult Index()
+        {
+            List<tblemp> emp = new List<tblemp>(); 
+              emp  = _dbContext.tblemps.ToList();
+            return View(emp);
+        } 
+
+    }
+}
---
+++ MVCCrudWithEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCrudWithEF.MvcApplication" Language="C#" %>
